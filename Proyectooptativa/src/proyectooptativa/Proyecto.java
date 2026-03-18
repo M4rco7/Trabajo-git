92a3f4831f93f8d7dc64cee259ef3570d8d400da
M4rco7 <marco.mig.garcia@gmail.com>
2026-03-18 11:39:21 +0100

    cambie el Proyecto.java

--- Proyectooptativa/src/proyectooptativa/Proyecto.java
+++ Proyectooptativa/src/proyectooptativa/Proyecto.java
@@ -3,7 +3,7 @@ package proyectooptativa;
 public class Proyecto {
 
 	public static void main(String[] args) {
-		System.out.print("este es mi ejemplo");
+		System.out.print("este es mi cam");
 
 	}
 
